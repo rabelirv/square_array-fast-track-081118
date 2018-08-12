2794b724ae462f46fea4be996bef87cc031dc48b
Irving E Rabel <rabel606@gmail.com>
2018-08-12 18:31:29 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,9 @@
 def square_array(array)
   # your code here
   
-  return_array = []
  
-  array.each do |numbers| 
-  #square mu nukbers
-  return_array.push num*num 
-  
-  #add the result to the return array 
+  array.map do |num| 
+   num*num 
   end 
-return_array
 
 end
