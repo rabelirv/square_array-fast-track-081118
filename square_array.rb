86d03b82333288c09e23659809ae75ff8cd4d561
Irving E Rabel <rabel606@gmail.com>
2018-08-12 18:21:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
   # your code here
-  array.each {|numbers| 
   
+  return_array[]
+ 
+  array.each do |numbers| {
+  #square mu nukbers
+  
+  #add the result to the return array 
   counter 
   
   array = (1,2,3,4)
