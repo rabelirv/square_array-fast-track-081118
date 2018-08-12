e4ff0eca352c03bfb84f65f4dd85db1c875f46bf
Irving E Rabel <rabel606@gmail.com>
2018-08-12 18:40:19 +0000

    automatically backed up by learn

---
+++ afternoon_routines.rb
@@ -0,0 +1,9 @@
+
+
+# hOw can we write two lines of code to print a routine for every day of the week?
+days_array = ["Sunday",'Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+day_array.each do |day|
+  puts "Today is#{day}"
+  print_routine(day)
+end 
